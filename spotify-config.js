986951667d40b46545dbfa39ea633d1fa8bb283d
nosserb <guylann.bresson.gb@gmail.com>
2026-03-27 17:27:37 +0100

    fix: fix redirection complete

--- spotify-config.js
+++ spotify-config.js
@@ -1,5 +1,6 @@
 window.SPOTEUR_CONFIG = {
 	spotifyClientId: "1418bd40adb94fc296ae25dbf93c7372",
-	redirectUri: "https://nosserb.github.io/WithMe/",
+	redirectUri: "https://nosserb.github.io/WithMe/login.html",
+	postLoginRedirect: "https://nosserb.github.io/WithMe/",
 	eventAPIkey: "2cab39d6-323f-44f8-a6b9-9d78c0e61d0d"
 };
