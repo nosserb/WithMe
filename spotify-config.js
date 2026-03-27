47e0540a1b06bc03d5d16eb07f4b9e93e802d310
nosserb <guylann.bresson.gb@gmail.com>
2026-03-27 17:21:29 +0100

    fix: fix redirection after log in

--- spotify-config.js
+++ spotify-config.js
@@ -1,5 +1,5 @@
 window.SPOTEUR_CONFIG = {
 	spotifyClientId: "1418bd40adb94fc296ae25dbf93c7372",
-	redirectUri: "http://127.0.0.1:5500/spoteur/login.html",
+	redirectUri: "https://nosserb.github.io/WithMe/",
 	eventAPIkey: "2cab39d6-323f-44f8-a6b9-9d78c0e61d0d"
 };
