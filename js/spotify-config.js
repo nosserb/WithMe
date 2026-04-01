19053cad5d0a3b54a14ebe8e4b9199b22ecd2b0b
nosserb <guylann.bresson.gb@gmail.com>
2026-04-01 12:57:38 +0200

    fix: fix redirection URI

--- js/spotify-config.js
+++ js/spotify-config.js
@@ -1,8 +1,8 @@
 window.WITHME_CONFIG = {
 	spotifyClientId: "1418bd40adb94fc296ae25dbf93c7372",
-	spotifyRedirectUri: "https://127.0.0.1:3443/html/login.html",
-	redirectUri: "https://127.0.0.1:3443/html/login.html",
-	localRedirectUri: "https://127.0.0.1:3443/html/login.html",
+	spotifyRedirectUri: "https://nosserb.github.io/WithMe/html/login.html",
+	redirectUri: "https://nosserb.github.io/WithMe/html/login.html",
+	localRedirectUri: "https://nosserb.github.io/WithMe/html/login.html",
 	postLoginRedirect: "https://127.0.0.1:3443/html/index.html",
 	apiBaseUrl: "",
 	TicketmasterKey: "eD59GweBRt9SXEpsiPs87U6RJGHw0CL8",
